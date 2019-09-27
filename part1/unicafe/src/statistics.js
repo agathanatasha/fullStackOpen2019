f846cef7b1255a8429834caa10d5e375aa47dcb4
Natasha Ho <natashaho1@gmail.com>
2019-09-27 15:41:50 -0400

    Empty state for Statistics component completed 1.9

--- part1/unicafe/src/statistics.js
+++ part1/unicafe/src/statistics.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
-const Statistics = ({good, neutral, bad, average, positivePercentage}) => {
+const Statistics = ({ good, neutral, bad, average, positivePercentage }) => {
+    const isNoFeedback = 
+        (good === 0 && neutral === 0 && bad === 0) ? true : false
+
+    const displayResult = () => {
+        if (isNoFeedback) {
+            return (
+                <p>No feedback given</p>
+            )
+        } else {
+            return (
+                <div>
+                    <p>good {good}</p>
+                    <p>neutral {neutral}</p>
+                    <p>bad {bad}</p>
+                    <p>average {average}</p>
+                    <p>positive {positivePercentage}%</p>
+                </div>
+            )
+        }
+    }
+
     return (
         <div>
             <h1>Statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>average {average}</p>
-            <p>positive {positivePercentage}%</p>
+            {displayResult()}
         </div>
     )
 }
