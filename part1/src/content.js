b58b87594791bdfd3b0fe6e628df993ab735f8ba
Natasha Ho <natashaho1@gmail.com>
2019-09-27 12:16:00 -0400

    completed 1.3

--- part1/src/content.js
+++ part1/src/content.js
@@ -4,9 +4,9 @@ import { Part } from './part';
 const Content = (props) => {
     return (
         <div>
-            <Part name={props.part1} exercises={props.exercises1} />
-            <Part name={props.part2} exercises={props.exercises2} />
-            <Part name={props.part3} exercises={props.exercises3} />
+            <Part name={props.part1.name} exercises={props.part1.exercises} />
+            <Part name={props.part2.name} exercises={props.part2.exercises} />
+            <Part name={props.part3.name} exercises={props.part3.exercises} />
         </div>
     )
 }
