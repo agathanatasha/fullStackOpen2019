8bb08e3c004ec52db8fea14a67eceae455df17b4
Natasha Ho <natashaho1@gmail.com>
2019-09-27 15:07:54 -0400

    completed 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,10 +5,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const totalClick = good + neutral + bad
+  const average = (good - bad)/totalClick
+  const positivePercentage = good/totalClick * 100
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>Statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>average {average}</p>
+        <p>positive {positivePercentage}%</p>
     </div>
   )
 }
