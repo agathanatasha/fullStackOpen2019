e257c87300758d2c95399ed3cd2baa68aa64723a
Natasha Ho <natashaho1@gmail.com>
2019-10-07 17:32:20 -0400

    fix misplaced script in package.json get persons state from db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,12 +5,7 @@ import { PersonForm } from './personForm';
 import { Filter } from './filter';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newPhoneNumber, setNewPhoneNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
