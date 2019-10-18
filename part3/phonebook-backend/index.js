299430daec81b4bf317500f0b37a1ebc2b5bd2e0
Natasha Ho <natashaho1@gmail.com>
2019-10-18 17:28:18 -0400

    implemented /info page completed 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  let date = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date.toString()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
