c5f77fc3247eb6e788c306c1586d7565e6595893
Natasha Ho <natashaho1@gmail.com>
2019-11-02 21:27:01 -0400

    installed morgan as middleware for logging completed 3.7 and 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,42 +1,50 @@
 const express = require("express")
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require("morgan")
 
 app.use(bodyParser.json())
+morgan.token('reqBody', function getReqBody(req) {
+  return JSON.stringify(req.body)
+});
+const loggerFormat = ':method :url :status :res[content-length] - :response-time ms - :reqBody'
+app.use(morgan(loggerFormat))
 
 let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-    {
-      "name": "Poppy Denny",
-      "number": "278379-122",
-      "id": 5
-    }
-  ]
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  {
+    "name": "Poppy Denny",
+    "number": "278379-122",
+    "id": 5
+  }
+]
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  // morgan(':method :url :status :res[content-length] - :response-time ms')
+  res.json(persons)
 })
 
 app.get('/api/persons/:id', (req, res) => {
+  morgan('tiny')
   const id = Number(req.params.id)
   const person = persons.find(person => person.id == id)
 
@@ -51,18 +59,18 @@ const generateId = (max) => {
   return Math.floor(Math.random() * Math.floor(max))
 }
 
-app.post('/api/persons', (req,res) => {
+app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if(!body.number){
+  if (!body.number) {
     return res.status(400).json({ error: 'missing number' })
-  } 
+  }
 
-  if(!body.name){
+  if (!body.name) {
     return res.status(400).json({ error: 'missing name' })
   }
 
-  if(persons.find(person => person.name == body.name)){
+  if (persons.find(person => person.name == body.name)) {
     return res.status(400).json({ error: 'name already exists' })
   }
 
@@ -91,5 +99,5 @@ app.get('/info', (req, res) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
